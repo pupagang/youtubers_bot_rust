d4d905ec91fb4fda83dbbb1931dd7dfbd23cbe01
billaids <jimmy.nelle@hsw-stud.de>
2021-07-19 07:45:29 +0200

    gdrive: read saved token on disk
    Signed-off-by: billaids <jimmy.nelle@hsw-stud.de>

--- src/gdrive.rs
+++ src/gdrive.rs
@@ -1,31 +1,48 @@
 use google_drive3::DriveHub;
 use hyper_rustls;
+use std::path::Path;
 use yup_oauth2;
 use yup_oauth2::InstalledFlowReturnMethod;
-
 pub struct Drive {
     pub hub: DriveHub,
 }
 
 impl Drive {
     pub async fn init(persist_path: &str) -> Self {
-        let secret = yup_oauth2::read_application_secret(persist_path)
+        if !Path::new("token.json").exists() {
+            let secret = yup_oauth2::read_application_secret(persist_path)
+                .await
+                .expect("failed to read \"credentials.json\" file");
+            let authenticator = yup_oauth2::InstalledFlowAuthenticator::builder(
+                secret,
+                InstalledFlowReturnMethod::Interactive,
+            )
+            .persist_tokens_to_disk("test.json")
+            .build()
             .await
-            .expect("failed to read \"credentials.json\" file");
-        let authenticator = yup_oauth2::InstalledFlowAuthenticator::builder(
-            secret,
-            InstalledFlowReturnMethod::Interactive,
-        )
-        .persist_tokens_to_disk("test.json")
-        .build()
-        .await
-        .expect("failed to create authenticator");
+            .expect("failed to create authenticator");
 
-        let hub = DriveHub::new(
-            hyper::Client::builder().build(hyper_rustls::HttpsConnector::with_native_roots()),
-            authenticator,
-        );
-        Self { hub }
+            let hub = DriveHub::new(
+                hyper::Client::builder().build(hyper_rustls::HttpsConnector::with_native_roots()),
+                authenticator,
+            );
+            Self { hub }
+        } else {
+            let secret = yup_oauth2::read_application_secret(persist_path)
+                .await
+                .expect("failed to read \"credentials.json\" file");
+            let authenticator = yup_oauth2::DeviceFlowAuthenticator::builder(secret)
+                .persist_tokens_to_disk("test.json")
+                .build()
+                .await
+                .expect("failed to create authenticator");
+
+            let hub = DriveHub::new(
+                hyper::Client::builder().build(hyper_rustls::HttpsConnector::with_native_roots()),
+                authenticator,
+            );
+            Self { hub }
+        }
     }
 
     pub async fn get_shared_drives(&self) -> google_drive3::api::DriveList {
